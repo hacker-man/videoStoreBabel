2fddb09d3afcab08d49b161e1bc70d9871df8dd6
Jesus Velasco <jesus.velasco@babel.es>
2016-03-30 14:08:02 +0200

    Terminado login basico

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,14 @@
 // Defino el módulo "movieRent"
-angular.module("movieRent", []);
+angular.module("movieRent", ['ngRoute']).config(
+    ["$routeProvider", "paths", function ($routeProvider, paths){
+
+        // Configuración de Route Provider
+        $routeProvider.when(paths.login, {
+            templateUrl: 'views/Login.html'
+        }).when(paths.movies, {
+            templateUrl: 'views/Movies.html'
+        }).otherwise({
+            templateUrl: 'views/404.html'
+        })
+    }]
+);
--- scripts/controllers/LoginController.js
+++ scripts/controllers/LoginController.js
@@ -1,6 +1,20 @@
 angular.module("movieRent").controller("LoginController",
-    ["$scope", function ($scope){
-        $scope.user = "";
-        $scope.pass = "";
+    ["$scope", "$window", "paths", function ($scope, $window, paths){
+
+
+        $scope.model = {
+            user: "",
+            pass: ""
+        };
+
+        $scope.next = function(){
+            console.log($scope);
+            if ($scope.model.user != "" && $scope.model.pass != ""){
+                 //= "#/movies";
+                 console.log("Estoy dentro", $scope.model.user, $scope.model.pass);
+                 var url = "/#" + paths.movies;
+                 $window.location.href = url; 
+            }
+        };
     }]
 );
